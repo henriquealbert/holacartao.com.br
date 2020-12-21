8d24bac26f680d5b5e39a814b8863852382e6c8b
Henrique <ishenriquealbert@gmail.com>
2020-12-21 18:34:15 -0300

    finished modelos page

--- src/components/ModelosList/singleModel.js
+++ src/components/ModelosList/singleModel.js
@@ -1,11 +1,9 @@
 import { Box, Img, Text } from '@chakra-ui/react';
 import Link from 'next/link';
-import { useState } from 'react';
+import styled from '@emotion/styled';
 
 export default function SingleModel({ model }) {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  const [overlay, setOverlay] = useState(false);
 
   return (
     <Box key={model.id} mb="24px">
@@ -20,34 +18,54 @@ export default function SingleModel({ model }) {
         }}
       >
         <a>
-          <Box maxW={{ base: '100%', md: '450px' }} position="relative">
+          <Wrapper>
+            <Box className="show-overlay">
+              <Text>Editar Modelo</Text>
+            </Box>
             <Img
               alt={model.title}
               title={model.title}
               src={API_URL + model.example_image.url}
-              width={450}
-              height={360}
-              onMouseEnter={() => setOverlay(true)}
-              onMouseLeave={() => setOverlay(false)}
+              w={{ base: '100%', md: '450px' }}
+              h={{ base: 'auto', md: '360px' }}
             />
-            <Box
-              display={overlay ? 'flex' : 'none'}
-              position="absolute"
-              top="50%"
-              left="50%"
-              transform="translate(-50%, -50%)"
-              bg="linear-gradient(180deg, rgba(17, 17, 15, 0.85) 0%, rgba(255, 255, 255, 0.67) 100%)"
-              w="100%"
-              h="100%"
-              justifyContent="center"
-              alignItems="center"
-              transition="all 250ms ease-in-out"
-            >
-              <Text fontSize="22px">Editar Modelo</Text>
-            </Box>
-          </Box>
+          </Wrapper>
         </a>
       </Link>
     </Box>
   );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+
+  &:hover {
+    .show-overlay {
+      visibility: visible;
+      display: flex;
+    }
+  }
+
+  .show-overlay {
+    display: none;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(
+      180deg,
+      rgba(17, 17, 15, 0.85) 0%,
+      rgba(255, 255, 255, 0.67) 100%
+    );
+    font-size: 22px;
+    line-height: 36px;
+    text-align: center;
+    letter-spacing: 0.01em;
+    visibility: hidden;
+    transition: all 200ms ease-in-out;
+  }
+`;
