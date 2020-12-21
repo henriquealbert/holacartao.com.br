0e2af85c5b805ba6308c7b01cc83ca6a1118ecf0
Henrique <ishenriquealbert@gmail.com>
2020-12-21 20:16:59 -0300

    modal isMobile

--- src/components/ModelosList/index.js
+++ src/components/ModelosList/index.js
@@ -1,12 +1,32 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure
+} from '@chakra-ui/react';
 
 import SingleModel from './SingleModel';
 import SelectCategory from './SelectCategory';
 import Pagination from './Pagination';
+import useWindowDimensions from '@/utils/getWindowDimensions';
 
 export default function ModelosList({ data }) {
+  //modal mobile
+  const { isMobile } = useWindowDimensions();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  //data
   const { cardModels } = data;
   const { cardCategories } = data;
 
@@ -96,10 +116,20 @@ export default function ModelosList({ data }) {
         >
           {categoria === 'All'
             ? currentModelsAll?.map((model) => (
-                <SingleModel key={model.id} model={model} />
+                <SingleModel
+                  key={model.id}
+                  model={model}
+                  cancelClick={isMobile}
+                  onOpen={onOpen}
+                />
               ))
             : categoriesList?.map((model) => (
-                <SingleModel key={model.id} model={model} />
+                <SingleModel
+                  key={model.id}
+                  model={model}
+                  cancelClick={isMobile}
+                  onOpen={onOpen}
+                />
               ))}
         </Flex>
 
@@ -110,6 +140,38 @@ export default function ModelosList({ data }) {
           lastPageCategory={lastPageCategory}
         />
       </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="xs" isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mb="46px">
+            <Icon viewBox="0 0 63 63" w={16} h={16} color="brand.200" mb="2rem">
+              <path
+                d="M27.962 5.193C13.647 5.193 2 16.839 2 31.155c0 14.316 11.647 25.963 25.962 25.963 14.316 0 25.963-11.647 25.963-25.963S42.278 5.193 27.963 5.193zm0 46.732c-11.452 0-20.77-9.318-20.77-20.77s9.318-20.77 20.77-20.77c11.453 0 20.77 9.318 20.77 20.77s-9.317 20.77-20.77 20.77z"
+                fill="currentColor"
+              />
+              <path
+                d="M35.305 38.497a10.54 10.54 0 01-1.535 1.27 10.363 10.363 0 01-3.71 1.56 10.665 10.665 0 01-4.195 0c-.66-.135-1.314-.34-1.945-.602a10.807 10.807 0 01-1.763-.956 10.68 10.68 0 01-2.809-2.809l-4.304 2.9c.558.828 1.199 1.607 1.902 2.308a15.557 15.557 0 004.956 3.341c.951.398 1.932.704 2.927.907a15.688 15.688 0 006.27 0c.991-.2 1.973-.506 2.928-.909a15.603 15.603 0 006.854-5.647l-4.304-2.9a10.393 10.393 0 01-1.272 1.537zM18.876 31.155a3.894 3.894 0 100-7.789 3.894 3.894 0 000 7.79zM37.05 25.962c-5.193 0-6.491 5.193-6.491 5.193h12.98s-1.3-5.193-6.49-5.193z"
+                fill="currentColor"
+              />
+            </Icon>
+            <Text fontSize="18px">
+              Para prosseguir use um aparelho tablet ou computador.
+            </Text>
+            <Text fontSize="14px" color="gray.400">
+              (Ou uma tela maior ou igual a 768px)
+            </Text>
+          </ModalBody>
+
+          <ModalFooter justifyContent="center" mb="2rem">
+            <Button variant="primary" onClick={onClose} type="button" size="lg">
+              Ok, Entendi!
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
--- src/components/ModelosList/singleModel.js
+++ src/components/ModelosList/singleModel.js
@@ -1,37 +1,40 @@
 import { Box, Img, Text } from '@chakra-ui/react';
-import Link from 'next/link';
 import styled from '@emotion/styled';
+import Router from 'next/router';
 
-export default function SingleModel({ model }) {
+export default function SingleModel({ model, cancelClick, onOpen }) {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-  return (
-    <Box key={model.id} mb="24px">
-      <Link
-        href={{
-          pathname: `/editor/${model.slug}`,
+  const onClick = () => {
+    if (cancelClick) {
+      onOpen();
+      return;
+    }
+    Router.push({
+      pathname: `/editor/${model.slug}`,
+      query: {
+        url: `/editor/${model.slug}`,
+        card: model.id
+      }
+    });
+  };
 
-          query: {
-            url: `/editor/${model.slug}`,
-            card: model.id
-          }
-        }}
-      >
-        <a>
-          <Wrapper>
-            <Box className="show-overlay">
-              <Text>Editar Modelo</Text>
-            </Box>
-            <Img
-              alt={model.title}
-              title={model.title}
-              src={API_URL + model.example_image.url}
-              w={{ base: '100%', md: '450px' }}
-              h={{ base: 'auto', md: '360px' }}
-            />
-          </Wrapper>
-        </a>
-      </Link>
+  return (
+    <Box key={model.id} mb="24px" onClick={onClick} cursor="pointer">
+      <a>
+        <Wrapper>
+          <Box className="show-overlay">
+            <Text textDecoration="underline">Editar Modelo</Text>
+          </Box>
+          <Img
+            alt={model.title}
+            title={model.title}
+            src={API_URL + model.example_image.url}
+            w={{ base: '100%', md: '450px' }}
+            h={{ base: 'auto', md: '360px' }}
+          />
+        </Wrapper>
+      </a>
     </Box>
   );
 }
@@ -58,8 +61,8 @@ const Wrapper = styled.div`
     align-items: center;
     background: linear-gradient(
       180deg,
-      rgba(17, 17, 15, 0.85) 0%,
-      rgba(255, 255, 255, 0.67) 100%
+      rgba(17, 17, 15, 1) 0%,
+      rgba(255, 255, 255, 0.47) 100%
     );
     font-size: 22px;
     line-height: 36px;
