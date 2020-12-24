424af6d7e17d6cecf5f2a7c6bef8d28e8c06b218
Henrique <ishenriquealbert@gmail.com>
2020-12-24 18:14:32 -0300

    fixed lost pass and recover acc

--- src/components/FormLostPass/index.js
+++ src/components/FormLostPass/index.js
@@ -1,28 +1,56 @@
-import client from '@/graphql/client';
-import { ErrorMessage, Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
 import { useState } from 'react';
-import MUTATION_FORGOT_PASS from '@/graphql/mutations/forgotPassword';
+import { Formik, Form, Field } from 'formik';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  Icon,
+  Input,
+  Text,
+  useToast
+} from '@chakra-ui/react';
+import * as Yup from 'yup';
+import Router from 'next/router';
 
-import * as S from './styled';
+import client from '@/graphql/client';
+import MUTATION_FORGOT_PASS from '@/graphql/mutations/forgotPassword';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
 
 export default function FormLostPass() {
+  const toast = useToast();
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [success, setSuccess] = useState(false);
 
-  const handleSubmit = async ({ email }) => {
+  const handleSubmit = async ({ email }, { resetForm }) => {
     setLoading(true);
 
     try {
       await client.request(MUTATION_FORGOT_PASS, {
-        email: email
+        email
       });
       setLoading(false);
-      setSuccess(true);
+      toast({
+        title: 'E-mail de recuperação enviado!',
+        description:
+          'Acesse seu e-mail e siga as instruções para recuperar sua conta.',
+        status: 'info',
+        duration: 9000,
+        isClosable: true
+      });
+      setTimeout(() => {
+        Router.push('/criar-nova-senha/');
+      }, 9000);
     } catch (e) {
       setLoading(false);
-      setError(e);
+      toast({
+        title: 'Erro ao realizar sua solicitação.',
+        description: 'Este e-mail não existe em nossa base de dados.',
+        status: 'error',
+        duration: 9000,
+        isClosable: true
+      });
+      resetForm();
     }
   };
 
@@ -32,53 +60,108 @@ export default function FormLostPass() {
       .required('O e-mail é um campo obrigatório.')
   });
 
-  if (success) {
-    return (
-      <S.Wrapper>
-        <p>Acesse seu e-mail para Recuperar sua Conta!</p>
-      </S.Wrapper>
-    );
-  }
-
   return (
-    <S.Wrapper>
-      <h1>Esqueceu sua senha?</h1>
-      <p>Digite o e-mail atrelado a sua conta para recuperar sua senha.</p>
-      <Formik
-        initialValues={{
-          email: ''
-        }}
-        validationSchema={validations}
-        onSubmit={handleSubmit}
+    <Box
+      my="4rem"
+      mx={{ base: '24px', md: 'auto' }}
+      maxW={{ base: 'none', md: '1280px' }}
+    >
+      <Flex
+        direction={{ base: 'column-reverse', md: 'row' }}
+        justifyContent={{ base: 'none', md: 'center' }}
       >
-        {() => (
-          <Form>
-            <S.Erro>
-              {error ? (
-                <span>
-                  <strong>Erro ao realizar sua solicitação.</strong>
-                  <small>Este E-mail não existe em nossa base de dados.</small>
-                </span>
-              ) : (
-                ''
-              )}
-            </S.Erro>
+        <Text
+          as="h1"
+          letterSpacing="0.01em"
+          color="gray.900"
+          fontWeight="500"
+          fontSize={{ base: '35px', md: '56px' }}
+          lineHeight={{ base: '55px', md: '66px' }}
+          mt={{ base: '40px', md: '0' }}
+          maxW={{ base: 'none', md: '300px' }}
+          mr={{ base: '0', md: '100px' }}
+        >
+          Esqueceu sua Senha?
+        </Text>
+        <Icon color="brand.200" viewBox="0 0 100 100" w={100} h={100}>
+          <path
+            d="M50 8.333C27.025 8.333 8.334 27.025 8.334 50S27.024 91.667 50 91.667c22.975 0 41.667-18.692 41.667-41.667S72.975 8.333 50 8.333zm0 75C31.621 83.333 16.667 68.38 16.667 50c0-18.38 14.954-33.333 33.333-33.333 18.38 0 33.334 14.954 33.334 33.333 0 18.38-14.955 33.333-33.334 33.333z"
+            fill="currentColor"
+          />
+          <path
+            d="M35.416 50a6.25 6.25 0 100-12.5 6.25 6.25 0 000 12.5zM64.554 49.942a6.22 6.22 0 100-12.442 6.22 6.22 0 000 12.442zM50 58.333c-12.5 0-16.666 12.5-16.666 12.5h33.333S62.5 58.333 50 58.333z"
+            fill="currentColor"
+          />
+        </Icon>
+      </Flex>
 
-            <div>
-              <Field
-                name="email"
-                type="email"
-                placeholder="Digite seu e-mail"
-              />
-              <ErrorMessage component="span" name="email" />
-            </div>
-            <S.BtnSubmit type="submit">
-              {' '}
-              {loading ? 'Carregando... ' : 'Recuperar Conta'}
-            </S.BtnSubmit>
-          </Form>
-        )}
-      </Formik>
-    </S.Wrapper>
+      <Text
+        fontSize="18px"
+        color="gray.500"
+        mt="20px"
+        mb="50px"
+        maxW="500px"
+        mx={{ base: '0', md: 'auto' }}
+      >
+        Não se preoocupe! Digite o e-mail atrelado à sua conta para recuperar
+        sua senha. Em seguida clique em “enviar e-mail” e confira a sua caixa de
+        entrada.
+      </Text>
+      <Flex justifyContent={{ base: 'none', md: 'center' }}>
+        <Formik
+          initialValues={{
+            email: ''
+          }}
+          validationSchema={validations}
+          onSubmit={handleSubmit}
+        >
+          {() => (
+            <Box w={{ base: '100%', md: '325px' }}>
+              <Form>
+                <Field name="email">
+                  {({ field, form }) => (
+                    <FormControl
+                      mb="2rem"
+                      isInvalid={form.errors.email && form.touched.email}
+                    >
+                      <Input
+                        {...field}
+                        id="email"
+                        placeholder="Digite seu e-mail"
+                        size="lg"
+                        border="3px solid"
+                        borderColor="gray.500"
+                        h="50px"
+                        type="email"
+                      />
+                      <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  isLoading={loading}
+                  loadingText="Enviando..."
+                  size="lg"
+                  mb="2rem"
+                  w="100%"
+                >
+                  Enviar E-mail
+                </Button>
+              </Form>
+            </Box>
+          )}
+        </Formik>
+      </Flex>
+      <Button
+        variant="link"
+        onClick={() => Router.back()}
+        color="gray.900"
+        textTransform="normal"
+      >
+        <Icon as={ChevronLeftIcon} w={10} h={10} /> Voltar
+      </Button>
+    </Box>
   );
 }
--- src/components/FormRecoveryPass/index.js
+++ src/components/FormRecoveryPass/index.js
@@ -1,17 +1,31 @@
-import client from '@/graphql/client';
-import { ErrorMessage, Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  Icon,
+  Input,
+  Text,
+  useToast
+} from '@chakra-ui/react';
 import { useState } from 'react';
-import MUTATION_RESET_PASS from '@/graphql/mutations/resetPassword';
 import Router from 'next/router';
+import * as Yup from 'yup';
 
-import * as S from './styled';
+import client from '@/graphql/client';
+import MUTATION_RESET_PASS from '@/graphql/mutations/resetPassword';
 
 export default function FormRecoveryPass({ query }) {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const toast = useToast();
 
-  const handleSubmit = async ({ code, password, passwordConfirmation }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (
+    { code, password, passwordConfirmation },
+    { resetForm }
+  ) => {
     setLoading(true);
 
     try {
@@ -21,10 +35,27 @@ export default function FormRecoveryPass({ query }) {
         passwordConfirmation: passwordConfirmation
       });
       setLoading(false);
-      Router.push('/login/');
+      toast({
+        title: 'Conta recuperada!',
+        description: 'Sua senha foi alterada com sucesso!',
+        status: 'success',
+        duration: 9000,
+        isClosable: true
+      });
+      setTimeout(() => {
+        Router.push('/login/');
+      }, 9000);
     } catch (e) {
       setLoading(false);
-      setError(e);
+      toast({
+        title: 'Erro ao realizar sua solicitação.',
+        description:
+          'Token Inválido. Acesse a página de recuperação de conta para gerar um novo Token.',
+        status: 'error',
+        duration: 9000,
+        isClosable: true
+      });
+      resetForm();
     }
   };
 
@@ -43,55 +74,149 @@ export default function FormRecoveryPass({ query }) {
   });
 
   return (
-    <S.Wrapper>
-      <h1>Crie sua nova senha</h1>
-      <p>Digite o e-mail atrelado a sua conta para recuperar sua senha.</p>
-      <Formik
-        initialValues={{
-          code: query.code,
-          password: '',
-          passwordConfirmation: ''
-        }}
-        validationSchema={validations}
-        onSubmit={handleSubmit}
+    <Box
+      my="4rem"
+      mx={{ base: '24px', md: 'auto' }}
+      maxW={{ base: 'none', md: '1280px' }}
+    >
+      <Flex
+        direction={{ base: 'column-reverse', md: 'row' }}
+        justifyContent={{ base: 'none', md: 'center' }}
       >
-        {() => (
-          <Form>
-            <S.Erro>
-              {error ? (
-                <span>
-                  <strong>Erro ao realizar sua solicitação.</strong>
-                  <small>Token Inválido.</small>
-                </span>
-              ) : (
-                ''
-              )}
-            </S.Erro>
+        <Text
+          as="h1"
+          letterSpacing="0.01em"
+          color="gray.900"
+          fontWeight="500"
+          fontSize={{ base: '35px', md: '56px' }}
+          lineHeight={{ base: '55px', md: '66px' }}
+          mt={{ base: '40px', md: '0' }}
+          maxW={{ base: 'none', md: '350px' }}
+          mr={{ base: '0', md: '100px' }}
+        >
+          Recuperação de Senha
+        </Text>
+        <Icon color="brand.200" viewBox="0 0 100 100" w={100} h={100}>
+          <path
+            d="M50 8.333C27.025 8.333 8.334 27.025 8.334 50S27.024 91.667 50 91.667c22.975 0 41.667-18.692 41.667-41.667S72.975 8.333 50 8.333zm0 75C31.621 83.333 16.667 68.38 16.667 50c0-18.38 14.954-33.333 33.333-33.333 18.38 0 33.334 14.954 33.334 33.333 0 18.38-14.955 33.333-33.334 33.333z"
+            fill="currentColor"
+          />
+          <path
+            d="M35.416 50a6.25 6.25 0 100-12.5 6.25 6.25 0 000 12.5zM64.554 49.942a6.22 6.22 0 100-12.442 6.22 6.22 0 000 12.442zM50 75c16.666 0 20.833-16.667 20.833-16.667H29.167S33.332 75 50 75z"
+            fill="currentColor"
+          />
+        </Icon>
+      </Flex>
 
-            <div>
-              <Field name="code" type="text" placeholder="Token" />
-              <ErrorMessage component="span" name="code" />
-            </div>
-            <div>
-              <Field name="password" type="password" placeholder="Nova Senha" />
-              <ErrorMessage component="span" name="password" />
-            </div>
-            <div>
-              <Field
-                name="passwordConfirmation"
-                type="password"
-                placeholder="Cofirmar Senha"
-              />
-              <ErrorMessage component="span" name="passwordConfirmation" />
-            </div>
+      <Text
+        fontSize="18px"
+        color="gray.500"
+        mt="20px"
+        mb="2rem"
+        textAlign={{ base: 'left', md: 'center' }}
+      >
+        Insira o TOKEN que recebeu em seu e-mail e crie sua nova senha.
+      </Text>
 
-            <S.BtnSubmit type="submit">
-              {' '}
-              {loading ? 'Carregando... ' : 'Alterar Senha'}
-            </S.BtnSubmit>
-          </Form>
-        )}
-      </Formik>
-    </S.Wrapper>
+      <Flex justifyContent={{ base: 'none', md: 'center' }} minW="305px">
+        <Formik
+          initialValues={{
+            code: query.code ? query.code : '',
+            password: '',
+            passwordConfirmation: ''
+          }}
+          validationSchema={validations}
+          onSubmit={handleSubmit}
+          enableReinitialize
+        >
+          {() => (
+            <Box w={{ base: '100%', md: '325px' }}>
+              <Form>
+                <Field name="code">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.code && form.touched.code}
+                      mb="1rem"
+                      w="100%"
+                    >
+                      <Input
+                        {...field}
+                        id="code"
+                        placeholder="TOKEN"
+                        border="3px solid"
+                        borderColor="gray.500"
+                        h="50px"
+                      />
+                      <FormErrorMessage>{form.errors.code}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+
+                <Field name="password">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.password && form.touched.password}
+                      mb="1rem"
+                    >
+                      <Input
+                        {...field}
+                        id="password"
+                        placeholder="Nova Senha"
+                        border="3px solid"
+                        borderColor="gray.500"
+                        h="50px"
+                        type="password"
+                        w="100%"
+                      />
+                      <FormErrorMessage>
+                        {form.errors.password}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+
+                <Field name="passwordConfirmation">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={
+                        form.errors.passwordConfirmation &&
+                        form.touched.passwordConfirmation
+                      }
+                      mb="2rem"
+                    >
+                      <Input
+                        {...field}
+                        id="passwordConfirmation"
+                        placeholder="Cofirmar Senha"
+                        border="3px solid"
+                        borderColor="gray.500"
+                        h="50px"
+                        type="password"
+                        w="100%"
+                      />
+                      <FormErrorMessage>
+                        {form.errors.passwordConfirmation}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+
+                <Button
+                  variant="primary"
+                  type="submit"
+                  isLoading={loading}
+                  loadingText="Carregando..."
+                  size="lg"
+                  mb="2rem"
+                  w="100%"
+                >
+                  Alterar Senha
+                </Button>
+              </Form>
+            </Box>
+          )}
+        </Formik>
+      </Flex>
+    </Box>
   );
 }
