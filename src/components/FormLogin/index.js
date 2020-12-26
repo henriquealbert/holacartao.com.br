928f0c6c02eb36e03fe13db0a823f7cc5ad33c84
Henrique <ishenriquealbert@gmail.com>
2020-12-26 15:03:59 -0300

    login page

--- src/components/FormLogin/index.js
+++ src/components/FormLogin/index.js
@@ -1,21 +1,26 @@
-// Utils
 import Link from 'next/link';
 import React, { useState, useContext } from 'react';
+import { Formik, Form, Field } from 'formik';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  Input,
+  Text,
+  useToast
+} from '@chakra-ui/react';
+import * as Yup from 'yup';
+
 import { login } from '@/lib/auth';
 import AppContext from '@/Contexts/AppContext';
 
-// Components
-import { ErrorMessage, Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
-
-// Styles
-import * as S from './styled';
-
 export default function FormLogin() {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const toast = useToast();
 
-  const appContext = useContext(AppContext);
+  const { setUser } = useContext(AppContext);
 
   const validations = Yup.object().shape({
     identifier: Yup.string()
@@ -26,71 +31,134 @@ export default function FormLogin() {
       .required('A senha é um campo obrigatório.')
   });
 
-  const handleSubmit = (data) => {
+  const handleSubmit = ({ identifier, password }, { resetForm }) => {
     setLoading(true);
-    login(data.identifier, data.password)
+    login(identifier, password)
       .then((res) => {
         setLoading(false);
         // set authed User in global context to update header/app state
-        appContext.setUser(res.data.user);
+        setUser(res.data.user);
       })
-      .catch((error) => {
-        setError(error.response?.data);
+      .catch(() => {
         setLoading(false);
+        toast({
+          title: 'Erro ao realizar seu login.',
+          description: 'E-mail ou senha inválidos, tente novamente.',
+          status: 'error',
+          duration: 9000,
+          isClosable: true
+        });
+        resetForm();
       });
   };
 
   return (
-    <S.Wrapper>
-      <h1>bem-vindo de novo!</h1>
-      <S.NaoPossui>
-        ainda não possui uma conta?{' '}
-        <Link href="/criar-conta/">
-          <a>clique aqui</a>
-        </Link>
-      </S.NaoPossui>
-      <Formik
-        initialValues={{
-          identifier: '',
-          password: ''
-        }}
-        validationSchema={validations}
-        onSubmit={handleSubmit}
+    <Box
+      my="4rem"
+      mx={{ base: '24px', md: 'auto' }}
+      maxW={{ base: 'none', md: '1280px' }}
+    >
+      <Text
+        as="h1"
+        fontSize="45px"
+        lineHeight="48px"
+        fontWeight="500"
+        textAlign="center"
+        color="brand.200"
+        mb="1rem"
       >
-        {() => (
-          <Form>
-            <S.ErroNoLogin>
-              {error ? (
-                <span>
-                  <strong>Erro ao realizar seu login.</strong>
-                  <small>E-mail ou senha inválidos, tente novamente.</small>
-                </span>
-              ) : (
-                ''
-              )}
-            </S.ErroNoLogin>
+        Bom ver você novamente.
+      </Text>
 
-            <div>
-              <Field name="identifier" type="email" placeholder="E-mail" />
-              <ErrorMessage component="span" name="identifier" />
-            </div>
-            <div>
-              <Field name="password" type="password" placeholder="Senha" />
-              <ErrorMessage component="span" name="password" />
-            </div>
-            <S.BtnSubmit type="submit">
-              {loading ? 'Carregando... ' : 'Fazer Login'}
-            </S.BtnSubmit>
-          </Form>
-        )}
-      </Formik>
+      <Flex justifyContent={{ base: 'none', md: 'center' }} mt="2rem">
+        <Formik
+          initialValues={{
+            identifier: '',
+            password: ''
+          }}
+          validationSchema={validations}
+          onSubmit={handleSubmit}
+        >
+          {() => (
+            <Box w={{ base: '100%', md: '325px' }}>
+              <Form>
+                <Field name="identifier">
+                  {({ field, form }) => (
+                    <FormControl
+                      mb="1rem"
+                      isInvalid={
+                        form.errors.identifier && form.touched.identifier
+                      }
+                    >
+                      <Input
+                        {...field}
+                        id="identifier"
+                        placeholder="E-mail Cadastrado"
+                        size="lg"
+                        border="3px solid"
+                        borderColor="gray.500"
+                        h="50px"
+                        type="email"
+                      />
+                      <FormErrorMessage justifyContent="center">
+                        {form.errors.identifier}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="password">
+                  {({ field, form }) => (
+                    <FormControl
+                      mb="1rem"
+                      isInvalid={form.errors.password && form.touched.password}
+                    >
+                      <Input
+                        {...field}
+                        id="password"
+                        placeholder="Senha"
+                        size="lg"
+                        border="3px solid"
+                        borderColor="gray.500"
+                        h="50px"
+                        type="password"
+                      />
+                      <FormErrorMessage justifyContent="center">
+                        {form.errors.password}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
 
-      <p>
-        esqueceu sua senha?{' '}
-        <Link href="/esqueceu-senha/">
-          <a>clique aqui</a>
-        </Link>
-      </p>
-    </S.Wrapper>
+                <Text textAlign="center" color="gray.500" mb="2rem">
+                  Esqueceu sua senha?{' '}
+                  <Link href="/esqueceu-senha/">
+                    <Text
+                      as="a"
+                      cursor="pointer"
+                      _hover={{ textDecoration: 'underline' }}
+                      color="blue.500"
+                    >
+                      clique aqui
+                    </Text>
+                  </Link>
+                </Text>
+
+                <Button
+                  variant="primary"
+                  type="submit"
+                  isLoading={loading}
+                  loadingText="Carregando..."
+                  size="lg"
+                  mb="2rem"
+                  w="100%"
+                >
+                  Fazer Login
+                </Button>
+              </Form>
+            </Box>
+          )}
+        </Formik>
+      </Flex>
+    </Box>
   );
 }
