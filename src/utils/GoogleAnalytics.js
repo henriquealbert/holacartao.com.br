80b7c23201b46ca00265e048ff4960f5c250d2c9
Henrique <ishenriquealbert@gmail.com>
2021-01-06 22:21:19 -0300

    added google ads

--- src/utils/GoogleAnalytics.js
+++ src/utils/GoogleAnalytics.js
@@ -15,6 +15,21 @@ export default function GoogleAnalytics() {
             `
         }}
       />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-451436940"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-451436940');
+          `
+        }}
+      />
     </>
   );
 }
